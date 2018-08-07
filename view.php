2030e8481868c23a65a1845f59804bd400a66648
arnab8820 <42146043+arnab8820@users.noreply.github.com>
2018-08-07 03:05:57 +0000

    Add files via upload

---
+++ view.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  	<link rel="stylesheet" type="text/css" href="css/view.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	  <title></title>
+    <script type="text/javascript" src="js/view.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-inverse">
+  		<div class="container-fluid">
+  			<div class="navbar-header">
+  				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
+  					<span class="icon-bar"></span>
+  					<span class="icon-bar"></span>
+  					<span class="icon-bar"></span>
+  				</button>
+  				<a href="#" class="navbar-brand"><img src="img/logo.png"></a>
+  			</div>
+  			<div class="collapse navbar-collapse" id="MyNavbar">
+  				<ul class="nav navbar-nav navbar-right">
+  					<li class="active"><a href="index.html">Home</a></li>
+  					<li><a href="#">About Us</a></li>
+  					<li><a href="#">Upload</a></li>
+  					<li><a href="#">Contribute</a></li>
+  				</ul>
+  			</div>
+  		</div>
+  	</nav>
+
+    <!--nav section ends here -->
+
+    <!--Video section starts here-->
+    <div class="container-fluid">
+      <div class="video-container row">
+        <div  class="video col-lg-8" id="video">
+
+        </div>
+        <div class="details col-lg-4">
+          <div class="description">
+            <h2 id="title"></h2>
+            <br><h4 id="author"></h4>
+            <br><h4 id="views"></h4>
+          </div>
+        </div>
+      </div>
+      <div class="related-videos" id="related-videos">
+        <h3>You might also like</h3>
+      </div>
+    </div>
+</body>
+</html>

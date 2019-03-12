8421295c6705c20aad416dfbb0cf6ba4af83f3d6
Arnab <arnab8820@gmail.com>
2019-03-12 22:10:52 +0530

    final commit

--- creator.beta-testsite.tk/scripts/fetch_topic.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'connect.php';
-$sql = "select * from `topic_list`";
-$stmnt = $dbhandler->prepare($sql);
-$stmnt->execute();
-$data = $stmnt->fetchAll();
-echo(json_encode($data));
-?>
